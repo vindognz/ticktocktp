f05e4e56299d3ee307db51682268f7caf6188917
Vincent Rodley <vindognz@gmail.com>
2026-07-09 10:31:13 +1200

    lookup secrets by name

--- ticktocktp/totp.py
+++ ticktocktp/totp.py
@@ -2,14 +2,17 @@ import pyotp
 import time
 import store
 
-def get_code(secret: str):
-    totp = pyotp.TOTP(secret)
-    code = totp.now()
-    seconds_left = 30 - int(time.time()) % 30
-    return code, seconds_left
+def get_code(name: str):
+    try:
+        secrets = store.load()
+        totp = pyotp.TOTP(secrets[name])
+        code = totp.now()
+        seconds_left = 30 - int(time.time()) % 30
+        return code, seconds_left
+    except KeyError:
+        raise ValueError(f"No account found named {name}.")
 
-test_secret = "JBSWY3DPEHPK3PXP"
-code, seconds_left = get_code(test_secret)
+code, seconds_left = get_code("test")
 print(f"{code} {seconds_left}s left")
 print(store.config_dir())
 print(store.load())
