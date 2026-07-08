600c987e5746728d6d2e2c55d467bc67f97bc63e
Vincent Rodley <vindognz@gmail.com>
2026-07-09 09:57:52 +1200

    first totp code generation working

---
+++ ticktocktp/__init__.py
@@ -0,0 +1,1 @@
+"""ticktocktp - desktop TOTP client thingy"""
---
+++ ticktocktp/totp.py
@@ -0,0 +1,12 @@
+import pyotp
+import time
+
+def get_code(secret: str):
+    totp = pyotp.TOTP(secret)
+    code = totp.now()
+    seconds_left = 30 - int(time.time()) % 30
+    return code, seconds_left
+
+test_secret = "JBSWY3DPEHPK3PXP"
+code, seconds_left = get_code(test_secret)
+print(f"{code} {seconds_left}s left")
